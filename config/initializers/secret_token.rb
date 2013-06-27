6978c0e364ff0e36001fde076be42dd40ed65ded
Karen Caplan <keaslk@gmail.com>
2013-06-27 16:15:03 -0700

    Generated new rails app, DJBookr/line_up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LineUp::Application.config.secret_token = '34fdeb5bf3912bb05d1b578b625134f8b1989767d05babce8a5a9619abb6aeb615420d28aae30888bda1e4da4af90bfbbfd380d541010c7ed5ee2ec2aaff6930'
