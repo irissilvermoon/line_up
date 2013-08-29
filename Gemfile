1228d9232c651dc42fd2dafc660a23502f540230
Karen Caplan <keaslk@gmail.com>
2013-08-29 12:51:02 -0700

    added chosen gem for formatting Bookings form.

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
   gem 'zurb-foundation'
   gem 'jquery-ui-rails'
+  gem 'chosen-rails'
 end
 
 group :test, :development do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.ui.datepicker
 //= require jquery.timepicker
+//= require chosen-jquery
 //= require foundation
 //= require_tree .
 
