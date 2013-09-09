9ebb71a1ad3cfa0f267b6835caa5db083c9c1cad
Karen Caplan <keaslk@gmail.com>
2013-09-09 16:51:56 -0700

    changed config app file to not initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module LineUp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.asses.initialize_on_precompible = false
   end
 end
