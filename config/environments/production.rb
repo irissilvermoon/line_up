c910b68317bf2a118718ea76d4b1e8f5d60f0c22
Karen Caplan <keaslk@gmail.com>
2013-09-09 18:42:06 -0700

    :stmp => :smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ LineUp::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'djbookr.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
