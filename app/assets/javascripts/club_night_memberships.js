b64a0b0bf7186e55588fde30f6836aa57a2e2868
Karen Caplan <keaslk@gmail.com>
2013-09-12 15:37:11 -0700

    adding in modal code, still unworking

--- app/assets/javascripts/club_night_memberships.js
+++ app/assets/javascripts/club_night_memberships.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $('#new-member-link').on("click", function (event) {
-    event.stopPropegation();
+    event.stopPropagation();
     event.preventDefault();
 
     var link = $(event.currentTarget);
 
     console.log("link href:", link.attr('href'));
+
+    $(".membership-modal").foundation('reveal', 'open', { url: '#new-member-link'});
   });
 });
 
