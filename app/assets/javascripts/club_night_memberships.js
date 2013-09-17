0a47663e99ead62069748168c5606df088b17249
Karen Caplan <keaslk@gmail.com>
2013-09-17 11:14:10 -0700

    added modal to club nights page to show events

--- app/assets/javascripts/club_night_memberships.js
+++ app/assets/javascripts/club_night_memberships.js
@@ -1,17 +1,3 @@
-$(document).ready(function() {
 
-  // $('#new-member-link').on("click", function (event) {
-  //   event.stopPropagation();
-  //   event.preventDefault();
-
-  //   var link = $(event.currentTarget);
-
-  //   console.log("link href:", link.attr('href'));
-  //   // $("#new-member-link").foundation('reveal', 'open', {
-  //   //                                    url: link.attr('href')
-  //   //                                  });
-
-  // });
-});
 
 
