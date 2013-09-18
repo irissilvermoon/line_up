5d6131a669c212853c8f8620b96fd795b31c42a0
Karen Caplan <keaslk@gmail.com>
2013-09-17 22:20:46 -0700

    save! added to confirmation controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -8,7 +8,7 @@ class ConfirmationsController < ApplicationController
     @event = @time_slot.event
     @club_night = current_user.club_nights.find(@event.club_night.id)
     @time_slot.confirmed_by = current_user
-    @time_slot.save
+    @time_slot.save!
     head 200
   end
 
@@ -18,7 +18,7 @@ class ConfirmationsController < ApplicationController
     @club_night = current_user.club_nights.find(@event.club_night.id)
 
     @time_slot.confirmed_by = nil
-    @time_slot.save
+    @time_slot.save!
     head 200
   end
 end
